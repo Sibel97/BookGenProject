f8e3583dc31622e67a817791ca90623ac387d3b2
Sibel <s_hassan97@hotmail.co.uk>
2022-09-08 09:14:14 +0000

    Issue resolved

--- Front-end/application/routes.py
+++ Front-end/application/routes.py
@@ -11,7 +11,7 @@ def home():
 
 @app.route('/Generate', methods = ['GET'])
 def Generate():
-    Genre = requests.get('http://genre-api:5001/get_Genre').text
+    Genre = requests.get('http://Genre-api:5001/get_Genre').text
     Author = requests.get('http://Author-api:5002/get_Author').text
     result = requests.post('http://Book-api:5003/get_book', json = {"Author": Author, "Genre": Genre})
     return render_template('layout.html', Genre = Genre , Author = Author, result = result.text)
